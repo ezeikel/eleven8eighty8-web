e12de7b2a6be707785d462fc77a7cdd842bb5f10
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-03-18 11:04:49 +0000

    Added logo images.

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -2,28 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styled from 'styled-components';
+import logo from '../../assets/logo.png';
 
 
 const Wrapper = styled.header`
   display: grid;
-  grid-template-columns: 1fr;
-  align-items: center;
-  justify-content: space-between;
-  background-color: var(--color-white);
-  color: var(--color-black);;
-  h1 {
-    color: var(--color-black);
-    text-align: center;
-  }
-  @media (min-width: 792px) {
-    display: flex;
-  grid-template-columns: 300px 1fr;
-  }
+  justify-items: center;
 `;
 
 const Header = () => (
   <Wrapper>
-    <h1>Header</h1>
+    <img src={logo} />
   </Wrapper>
 );
 
